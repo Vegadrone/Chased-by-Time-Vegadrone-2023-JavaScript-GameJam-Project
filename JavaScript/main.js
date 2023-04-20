f613105162a8d3d485e4d7d018c4ec8459186e79
Vegadrone <spenaantonino@gmail.com>
2023-04-20 19:15:12 +0200

    feat: :sparkles: Project init

---
+++ JavaScript/main.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function(){
+    //Canvas Set Up
+    const canvas = document.getElementById("gameScreen");
+    const ctx = canvas.getContext('2d');
+
+    //Canvas dimensions
+    canvas.width = 500;
+    canvas.height = 500;
+
+});
