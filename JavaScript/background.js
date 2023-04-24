f8a7396bd5c3f16f354b906a51df1fc7ee969efa
Vegadrone <spenaantonino@gmail.com>
2023-04-24 20:07:12 +0200

    feat: :zap: adjust scrolling velocity

--- JavaScript/background.js
+++ JavaScript/background.js
@@ -11,11 +11,8 @@ class Layer {
     }
     
     update(){
-        if (this.x < -this.width){
-            this.x = 0;
-        } 
-        else {
-            this.x -= this.game.gameSpeed * this.speedModifier};  
+        if (this.x < -this.width) this.x = 0;
+        else this.x -= this.game.gameSpeed * this.speedModifier;  
     }
     
     draw(context){
@@ -36,20 +33,18 @@ export class Background {
         this.layer2image = document.getElementById('layer2');
         this.layer3image = document.getElementById('layer3');
         this.layer1image = document.getElementById('layer1');
-        this.layer1 = new Layer(this.game, this.imgWidth, this.imgHeight, 1, this.layer5image);
-        this.layer2 = new Layer(this.game, this.imgWidth, this.imgHeight, 0.8, this.layer4image);
-        this.layer3 = new Layer(this.game, this.imgWidth, this.imgHeight, 0.6, this.layer2image);
+        this.layer1 = new Layer(this.game, this.imgWidth, this.imgHeight, 1.5, this.layer5image);
+        this.layer2 = new Layer(this.game, this.imgWidth, this.imgHeight, 0.6, this.layer4image);
+        this.layer3 = new Layer(this.game, this.imgWidth, this.imgHeight, 1, this.layer2image);
         this.layer4 = new Layer(this.game, this.imgWidth, this.imgHeight, 0.4, this.layer3image);
-        this.layer5 = new Layer(this.game, this.imgWidth, this.imgHeight, 0.2, this.layer1image);
+        this.layer5 = new Layer(this.game, this.imgWidth, this.imgHeight, 0, this.layer1image);
         this.backgroundLayers = [this.layer5, this.layer4, this.layer2, this.layer3, this.layer1];
-        console.log(this.backgroundLayers);  
-        console.warn(this.layer1.g);
     }
 
     update(){
         this.backgroundLayers.forEach(layer => {
             layer.update();
-            console.log(this.backgroundLayers[0].x, this.backgroundLayers[0].y);
+    
         })
     }
 
